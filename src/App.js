b74ef2f712d75d16d98a03d0ddbab3240a621f9e
natenadze1993 <g.natenadze@myhome.ge>
2022-07-08 18:31:58 +0400

    123

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useFetchHook from './hooks/useFetchHook';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [tasks, setTasks] = useState([]);
+  const obj = {
+    url: 'https://my-third-project-48c3a-default-rtdb.firebaseio.com/tasks.json'
+  }
+
+  const data = useFetchHook(obj).then(x => x);
+  console.log(data)
+
+  useEffect(() => {
+
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
     <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={data}
+      />
     </React.Fragment>
   );
 }
